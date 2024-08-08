0ce6c822f3326bb0b181603dedf72ec24fcde5f0
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-09 00:26:52 +0600

    pyttsx3

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 
-
 recoginer = sr.Recognizer()
 engine = pyttsx3.init()
 
-def spcak(text):
-    engine.say("I will speak")
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
 if __name__ == "__main__":
+    speak("Hi i am chayan")
 
