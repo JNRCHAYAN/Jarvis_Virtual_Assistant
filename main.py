4f48c70dbdbaa4f2ee51b8e29db05bb1ef0d4b56
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-09 01:48:29 +0600

    Main work down

--- main.py
+++ main.py
@@ -9,43 +9,45 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-def processCommand(c):
-    # print(c)
-    if "open google" in c.lower():
+def processCommand(command):
+    # print(command)
+    if "open google" in command.lower():
         webbrowser.open("https://google.com")
-    elif "open youtube" in c.lower():
+    elif "open youtube" in command.lower():
         webbrowser.open("https://youtube.com")
-    elif "open facebook" in c.lower():
+    elif "open facebook" in command.lower():
         webbrowser.open("https://facebook.com")
-    elif "open github" in c.lower():
+    elif "open github" in command.lower():
         webbrowser.open("https://github.com")
-    elif "open gmail" in c.lower():
+    elif "open gmail" in command.lower():
         webbrowser.open("https://gmail.com")
-    
+    else:
+        speak("Sorry, I didn't understand the command.")
 if __name__ == "__main__":
-    speak("Start MY Bot.....")
+    speak("Starting my bot..")
     #Lisiten for the wake word Roy
     while True:
         r = sr.Recognizer()
-        
         try:
             with sr.Microphone() as source:
-             print("Lisiting...")
-             audio = r.listen(source, timeout=2, phrase_time_limit=1)
-            word = r.recognize_google(audio)
+                print("Lisiting...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio).lower()
             # print(command)
             # print("You Said" + r.recognize_sphinx(audio))
-            if (word.lower()== "roy"):
+            if word == "roy":
                 speak("Yes Sir")
+                print("Roy Active...")
+
                 #Lisiten for command
                 with sr.Microphone() as source:
-                    print("Roy Active...")
                     audio = r.listen(source)
                     command = r.recognize_google(audio)
                     processCommand(command)
-
+                    
+        except sr.UnknownValueError:
+            print("Sorry, I couldn't understand that.")
         except sr.RequestError as e:
-            print("Error : {0}".format(e))
-
+            print(f"Error: {e}")
 
 
