eb06fc3621e17f17d9d042c24d872aacc2cf16d5
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-09 13:49:39 +0600

    open ai add

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
 
-## This feature is paid user only
-from openai import OpenAI
+# ## This feature is paid user only
+# from openai import OpenAI
 
-client = OpenAI(
-    api_key= "#",
+# client = OpenAI(
+#     api_key= "#",
 
-)
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "You are a Virtual assistant named roy, skilled in General Task"},
-    {"role": "user", "content": "What is what is coding"}
-  ]
-)
+# )
+# completion = client.chat.completions.create(
+#   model="gpt-4o-mini",
+#   messages=[
+#     {"role": "system", "content": "You are a Virtual assistant named roy, skilled in General Task"},
+#     {"role": "user", "content": "What is what is coding"}
+#   ]
+# )
 
-print(completion.choices[0].message.content)
+# print(completion.choices[0].message.content)
 
 
